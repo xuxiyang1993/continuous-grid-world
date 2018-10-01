e221b08dccd7b0364c5d4fe8fd842e96fc702f19
xuxiyang1993 <xuxiyang1993@gmail.com>
2018-10-01 12:27:44 -0500

    update

--- simulator_with_intruder.py
+++ simulator_with_intruder.py
@@ -11,6 +11,7 @@ os.environ['SDL_VIDEO_WINDOW_POS'] = str(www[0]) + "," + str(www[1])
 pygame.init()
 white = (255, 255, 255)
 black = (0, 0, 0)
+red = (200, 0, 0)
 green = (0, 200, 0)
 
 # size of the airspace
@@ -21,8 +22,11 @@ gameIcon = pygame.image.load('images/intruder.png')
 pygame.display.set_icon(gameIcon)
 pygame.display.set_caption('Aircraft Guidance Simulator', 'Spine Runtime')
 
+intruder_size = 30  # number of intruder aircraft
+RADIUS = 16  # safe radius for aircraft: above this number will be considered conflict
 no_frame = 1  # one action can last how many frames or time steps?
 tick = 30  # update no more than 30 frames in one second
+min_dist = 128  # newly generated intruder shouldn't be too close to the ownship
 np.set_printoptions(precision=2)
 
 
@@ -38,6 +42,13 @@ def collision_wall(count):
     font = pygame.font.SysFont("comicsansms", 25)
     text = font.render("Collision with wall: " + str(count), True, black)
     screen.blit(text, (5, 30))
+
+
+# display how many conflicts there are at top right corner
+def collision_intruder(count):
+    font = pygame.font.SysFont("comicsansms", 25)
+    text = font.render("Conflict with intruder: " + str(count), True, red)
+    screen.blit(text, (5, 60))
 
 
 # display how many goals reached at top right corner
@@ -63,7 +74,7 @@ class DroneSprite(pygame.sprite.Sprite):
         vy = -self.speed * math.cos(self.rad)
         self.velocity = (vx, vy)
         # this aircraft will be safe if other aircraft are outside this radius
-        self.radius = 16
+        self.radius = RADIUS
         # the heading angle will be updated according to this delta_direction.
         self.delta_direction = 0
         # use keyboard to control left or right
@@ -96,6 +107,32 @@ class DroneSprite(pygame.sprite.Sprite):
         self.rect.center = self.position
 
 
+# the intruder aircraft sprite
+class PadSprite(pygame.sprite.Sprite):
+    def __init__(self, position, speed, direction):
+        pygame.sprite.Sprite.__init__(self)
+        self.src_image = pygame.image.load('images/intruder.png')
+        self.image = self.src_image
+        self.rect = self.src_image.get_rect()
+        self.position = position
+        self.speed = speed
+        self.direction = direction
+        self.rad = self.direction * math.pi / 180
+        vx = -self.speed * math.sin(self.rad)
+        vy = -self.speed * math.cos(self.rad)
+        self.velocity = (vx, vy)
+        self.radius = RADIUS
+
+    def update(self):
+        x, y = self.position
+        x += self.velocity[0]
+        y += self.velocity[1]
+        self.position = (x, y)
+        self.image = pygame.transform.rotate(self.src_image, self.direction)
+        self.rect = self.image.get_rect()
+        self.rect.center = self.position
+
+
 # the goal sprite for the ownship
 class GoalSprite(pygame.sprite.Sprite):
     def __init__(self, position):
@@ -115,32 +152,71 @@ class GoalSprite(pygame.sprite.Sprite):
 # pads is the list contains all the intruder aircraft sprite
 # own is the ownship we are controlling
 # goal is the goal sprite
-def get_state(own, goal):
+def get_state(pads, own, goal):
     state_list = []
-
-    state_list.append(own.position[0])
-    state_list.append(own.position[1])
-    state_list.append(own.velocity[0])
-    state_list.append(own.velocity[1])
-    state_list.append(own.direction)
+    for i in range(intruder_size):
+        state_list.append(pads[i].position[0])
+        state_list.append(pads[i].position[1])
+        state_list.append(pads[i].velocity[0])
+        state_list.append(pads[i].velocity[1])
+    for i in range(1):
+        state_list.append(own.position[0])
+        state_list.append(own.position[1])
+        state_list.append(own.velocity[0])
+        state_list.append(own.velocity[1])
+        state_list.append(own.direction)
     state_list.append(goal.position[0])
     state_list.append(goal.position[1])
 
     return np.array(state_list)
 
 
+# distance between two sprites
+def dist(a, b):
+    dx = a.position[0] - b.position[0]
+    dy = a.position[1] - b.position[1]
+    return math.sqrt(dx * dx + dy * dy)
+
+
+# reset the intruder aircraft position so that the intruder aircraft stays out of the safe distance
+# from the ownship
+# this usually happens when it's the beginning of the episode, when generating all the aircraft
+def reset_intruder(intruder):
+    # while the intruder is too near to the ownship, keep changing its position
+    while dist(drone, intruder) < min_dist:
+        intruder.position = (random.random() * width,
+                             random.random() * height)
+    intruder.speed = random.random() + 1
+    intruder.direction = random.random() * 360
+    intruder.rad = intruder.direction * math.pi / 180
+    vx = -intruder.speed * math.sin(intruder.rad)
+    vy = -intruder.speed * math.cos(intruder.rad)
+    intruder.velocity = (vx, vy)
+
+
 # generate a goal position
 goal = GoalSprite((random.random() * (width - 200) + 100,
                    random.random() * (height - 200) + 100))
 
+# pads is a list containts all the intruder aircraft
+pads = []
+for _ in range(intruder_size):
+    # generate position, speed, direction of new intruder aircraft.
+    pads.append(PadSprite((random.random() * width,
+                           random.random() * height),
+                          random.random() + 1,
+                          random.random() * 360))
 
 rect = screen.get_rect()  # screen center position
 
 # drone is the ownship we are controlling
 drone = DroneSprite(rect.center)
 drone_group = pygame.sprite.RenderPlain(drone)
+pad_group = pygame.sprite.RenderPlain(*pads)
 goal_group = pygame.sprite.RenderPlain(goal)
 
+import time
+time_list = []  # used to track running time of the algorithm
 
 time_step = -1  # this is the time step displayed at the top right corner.
 simulate = True
@@ -148,12 +224,16 @@ simulate = True
 while simulate:
     time_step += 1
 
-    current_state = get_state(drone, goal)
+    current_state = get_state(pads, drone, goal)
+    current_time = int(round(time.time() * 1000))  # time before the algorithm
 
     # there can plug in some algorithms, input is current_state, output is action
     # action = algorithm(current_state)
     # drone.delta_direction = action
     # drone.speed = action
+
+    time_after = int(round(time.time() * 1000))  # time after the algortihm
+    time_list.append(time_after - current_time)
 
     # use key board left and right to control the ownship
     for event in pygame.event.get():
@@ -182,26 +262,46 @@ while simulate:
                               random.random() * 200 + 300)
             drone.direction = random.random() * 360
 
+        # check if there is conflicts with other intruder aircraft
+        for intruder in pad_group:
+            x = intruder.position[0]
+            y = intruder.position[1]
+            if pygame.sprite.collide_circle(drone, intruder):
+                # there is conflict between the ownship and the current intruder
+                collide_intruder = True
+                drone.collision_intruder += 1
+                # reset the position of current intruder
+                reset_intruder(intruder)
+            elif x > width - 4 or x < 4 or y > height - 4 or y < 4:
+                # no conflict with the current intruder
+                intruder.position = drone.position
+                reset_intruder(intruder)
+
         # check if the ownship reaches the goal state.
         if pygame.sprite.collide_circle(drone, goal):
             # the ownship reaches the goal position
             collide_goal = True
             drone.collision_goal += 1
-
+            # reset the position of all the intruder aircraft
+            for intruder in pad_group:
+                reset_intruder(intruder)
             # generate a new goal for the aircraft
             goal.position = (random.random() * (width - 200) + 100,
                              random.random() * (height - 200) + 100)
 
         # update the drone, intruder aircraft, goal
         drone_group.update(deltat)
+        pad_group.update()
         goal_group.update()
 
         # draw the aircraft and the goal
+        pad_group.draw(screen)
         drone_group.draw(screen)
         goal_group.draw(screen)
 
         time_display(time_step)
         collision_wall(drone.collision_wall)
+        collision_intruder(drone.collision_intruder - intruder_size)
         collision_goal(drone.collision_goal)
 
         pygame.display.flip()
